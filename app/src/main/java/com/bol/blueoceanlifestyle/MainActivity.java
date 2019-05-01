cde17d918562e4253c60d448b8650a000a2fc999
srs39 <samanthashoecraft@gmail.com>
2019-05-01 15:07:42 -0700

    fixed symptoms to display symptoms fragment

--- app/src/main/java/com/bol/blueoceanlifestyle/MainActivity.java
+++ app/src/main/java/com/bol/blueoceanlifestyle/MainActivity.java
@@ -1,13 +1,16 @@
 package com.bol.blueoceanlifestyle;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
 
     @Override
@@ -19,11 +22,54 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ContactFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_contact);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_bp:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BPFragment()).commit();
+                break;
+            case R.id.nav_contact:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+            case R.id.nav_dosage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DosageFragment()).commit();
+                break;
+            case R.id.nav_mood:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MoodFragment()).commit();
+                break;
+            case R.id.nav_product:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProductFragment()).commit();
+                break;
+            case R.id.nav_symptoms:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SymptomsFragment()).commit();
+                break;
+
+            case R.id.nav_usage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new UsageFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
--- app/src/main/java/com/bol/blueoceanlifestyle/SymptomsFragment.java
+++ app/src/main/java/com/bol/blueoceanlifestyle/SymptomsFragment.java
@@ -11,6 +11,6 @@ public class SymptomsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_usage, container,false);
+        return inflater.inflate(R.layout.fragment_symptoms, container,false);
     }
 }
