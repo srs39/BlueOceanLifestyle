bb044e6c7f8ff4fd3b7b688b23614a24e3c860e2
srs39 <samanthashoecraft@gmail.com>
2019-05-06 13:59:54 -0700

    added activity for calender and made it available for selection. added comments to main Activity.

--- app/src/main/java/com/bol/cbdstatstracker/MainActivity.java
+++ app/src/main/java/com/bol/cbdstatstracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bol.cbdstatstracker;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,14 +11,17 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+/*This class is the main activity that handles the majority of the fragments and navigation drawer*/
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
 
+    /*Overrides default onCreate of super class creates new instance of main activity */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //checking for saved instance of app and setting main content
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //creates nave menu and toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,13 +32,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        //default to contact information if no saved instance
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new ContactFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_contact);
         }
     }
-
+    /*Overrides onNavigationItemSelected of super class switch triggered when menu item selected.
+     * chooses correct fragment or activity for display.
+     *  */
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()){
@@ -67,11 +74,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new UsageFragment()).commit();
                 break;
+            case R.id.nav_cal:
+                Intent calIntent = new Intent(this, CalenderActivity.class);
+                startActivity(calIntent);
+                break;
         }
+        //closes drawer after selection
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    /*Overrides onBackPressed of super class. will close navigation drawer if open.
+    * defaults to super, which closes app. need to add stack to go to previous fragment
+    * or activity.
+    * */
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
